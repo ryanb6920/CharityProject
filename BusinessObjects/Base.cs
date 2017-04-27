7385c69f768895e1ef50d5ecb3a6601e5214a86c
WalkingPube <r.s.b@hotmail.co.nz>
2017-04-27 20:31:16 +1200

    Added Business Object's classes

--- BusinessObjects/Base.cs
+++ BusinessObjects/Base.cs
@@ -9,7 +9,51 @@ namespace BusinessObjects
     public class Base
     {
         
+    }
 
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
 
+    public class Donors
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public class Charities
+    {
+        public int Id { get; set; }
+        public string CharityName { get; set; }
+        public string Email { get; set; }
+        public string URL { get; set; }
+        public string PhoneNo { get; set; }
+        public string OpenHours { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public class Donation
+    {
+        public int Id { get; set; }
+        public int DonorId { get; set; }
+        public string Category { get; set; }
+        public DateTime PickupDate { get; set; }
+        public int Recipient { get; set; }
+        public DateTime DropOffDate { get; set; }
+        public int NoItems { get; set; }
+        public int NoRecycledItems { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
--- DataAccessLayer/DAL.cs
+++ DataAccessLayer/DAL.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BusinessObjects;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DataAccessLayer
 {
     public class DAL
     {
+        public string ConnectionString = string.Empty;
+
+        public DAL()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["CharityProjectDB"].ConnectionString;
+        }
 
     }
 }
