ad3f5ecc0e1c32b890a39b027dd138e797935743
WalkingPube <r.s.b@hotmail.co.nz>
2017-04-29 00:56:23 +1200

    Added Globals class to project to handle which user is logged in where etc

---
+++ CharityProject_rbro752/Gobals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessObjects;
+
+namespace CharityProject_rbro752
+{
+    public static class Gobals
+    {
+        public static Donors SignedInDonor;
+        public static Charities SignedInCharity;
+        public static Users SignedInUser;
+    }
+}
